143ef78d6b25484df87b0b6f1feb7ae4150dd6e2
rajithabuttimani-source <rajithabuttimani@gmail.com>
2026-08-27 17:02:57 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyD-sqFyHkJPYYN9ghjZJdCubu-b0NZCkKc",
   authDomain: "civic-care-795a5.firebaseapp.com",
   projectId: "civic-care-795a5",
   storageBucket: "civic-care-795a5.firebasestorage.app",
   messagingSenderId: "746102469610",
   appId: "1:746102469610:web:7e48dd678af2423a427893"
+};
 };
 
 const app = initializeApp(firebaseConfig);
